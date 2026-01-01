aabc37068642d71d7036b76bb98df6fa51be697e
SHASHI KALWAR <138351592+shashishivgit909@users.noreply.github.com>
2026-01-01 23:28:28 +0530

    update frontend

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,3 @@
 // Base URL for the backend API
-export const BASE_URL = 'http://localhost:5000';
+// export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://contact-form-backend-4sot.onrender.com';
